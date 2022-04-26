a2f9a5048a88a1062f79b064ff09fd1325ba0c07
Gate <gate@ip-10-0-10-19.ec2.internal>
2022-04-26 20:00:25 +0000

    add shell script for auto run

---
+++ autorun.sh
@@ -0,0 +1,4 @@
+conda activate ifpriDisease
+python /home/gate/repos/MTID_MEDIA_ANALYSIS_CROP_DISEASE/ifpriDiseaseApp.py --config /home/gate/repos/MTID_MEDIA_ANALYSIS_CROP_DISEASE//diseaseTypeTrain.config --gpu --rss --outputFolder /home/gate/repos/MTID_MEDIA_ANALYSIS_CROP_DISEASE/output/ --model /home/gate/repos/MTID_MEDIA_ANALYSIS_CROP_DISEASE/trainedModel/
+sleep 10
+pkill screen
